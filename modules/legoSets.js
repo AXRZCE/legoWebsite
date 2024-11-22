baef2d4287a89db430337e5b5e4bdd7907c3e49e
AXRZCE <aksharaj.asp.15@gmail.com>
2024-11-22 09:20:01 -0500

    try--8

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,7 +1,6 @@
-// legoSets.js
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const pg = require('pg'); // Add this line
+require('pg'); // Add this line
 
 // Initialize Sequelize connection with SSL and specify 'dialectModule'
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
