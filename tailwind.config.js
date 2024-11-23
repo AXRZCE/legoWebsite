4213495706f8ca9706d6ae9fbd4ed580091a2cef
AXRZCE <aksharaj.asp.15@gmail.com>
2024-11-23 00:13:45 -0500

    a2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./views/**/*.ejs`], // all .html files
+  content: [`./views/**/*.html`], // all .html files
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'), 
-    require('daisyui')
-  ],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: ["fantasy","light","dark"] // Add your desired theme here (e.g., "fantasy", "light", "dark", etc.)
   }
