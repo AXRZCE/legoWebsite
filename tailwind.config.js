3fe9e3361f88fbfab559bd56adc8e893ce3dd683
AXRZCE <aksharaj.asp.15@gmail.com>
2024-11-22 23:45:23 -0500

    css7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './views/**/*.ejs',    // Watch for changes in .html files in the views folder
-    './public/**/*.ejs',   // Watch for changes in .html files in the public folder (if any)
-  ],
+  content: [`./views/**/*.ejs`], // all .html files
   theme: {
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'), 
